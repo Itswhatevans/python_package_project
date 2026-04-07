b3f1bb1040144fc1fcd6424d53bf0756c3f4f7b4
Trent Peterson <61849658+Trent-Peterson@users.noreply.github.com>
2026-04-07 14:12:07 -0600

    Update __init__.py

--- Rocket_Launches/__init__.py
+++ Rocket_Launches/__init__.py
@@ -1,1 +1,10 @@
-
+from .analysis import (
+    add_country,
+    launches_per_year,
+    status_by_year,
+    rocket_usage,
+    status_by_location,
+    launches_by_country,
+    run_failure_model,
+    evaluate_model
+)
